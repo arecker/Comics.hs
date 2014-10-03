56a79d4f896a88679faf178e0145b668f195570d
Alex Recker <alex@reckerfamily.com>
2014-10-02 21:48:40 -0500

    Progress... I think.

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@
 
 import Web.Scotty
 import Data.Monoid (mconcat)
+import Control.Monad (forM_)
 import Text.Blaze.Html5
 import Text.Blaze.Html5.Attributes
 import qualified Web.Scotty as S
@@ -13,15 +14,21 @@ import Text.Blaze.Html.Renderer.Text
 main = do
   scotty 3000 $ do
     get "/" $ do
-      S.html . renderHtml $ do
-        getHead
-        h1 "Home"
+      S.html . renderHtml $ do getHome
 
 
-getHead = do
+getHome :: Html
+getHome = do
   H.head $ do
     H.title "Comics"
     getBootstrap
+  H.body $ do
+    wrapInContainerFluid $ H.h1 "Home"
 
 
+getBootstrap :: Html
 getBootstrap = link ! href "http://bootswatch.com/journal/bootstrap.min.css" ! rel "stylesheet"
+
+wrapInContainerFluid :: Html -> Html
+wrapInContainerFluid html = do
+  H.div ! class_ "container-fluid" $ html
