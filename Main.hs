2396ab4c93ad22f3ba35e8fbbc62441bb35db472
Alex Recker <alex@reckerfamily.com>
2014-10-05 16:32:58 -0500

    Pages and placeholders

--- Main.hs
+++ Main.hs
@@ -16,19 +16,41 @@ main = do
     get "/" $ do
       S.html . renderHtml $ do getHome
 
-
 getHome :: Html
 getHome = do
   H.head $ do
-    H.title "Comics"
+    meta ! charset "UTF-8"
+    H.title "Comics, Man"
     getBootstrap
+    H.style ".main {max-width: 500px;margin-bottom: 3em;} .jumbotron {background-color: transparent;}"
   H.body $ do
-    wrapInContainerFluid $ H.h1 "Home"
+    H.div ! class_ "container-fluid main" $ do
+      getJumbotron
+      getNav
+      getLatest
+
+
+getJumbotron :: Html
+getJumbotron = H.div ! class_ "jumbotron" $ h1 "Comics, man."
+
+
+getNav :: Html
+getNav = do
+  H.div ! class_ "nav nav-pills" $ do
+    li ! class_ "active" $ a ! href "/" $ "Latest"
+    li $ a ! href "/everything/" $ "Everything"
+    li $ a ! href "/follow/" $ "Follow"
+
+
+getLatest :: Html
+getLatest = do
+  H.div ! class_ "page-header" $ do
+      h1 ! class_ "lead" $ "Mitochondrial Eve"
+      p "October 5, 2014"
+  img ! src "http://www.explosm.net/db/files/Comics/Dave/EVE.png" ! alt "" ! class_ "img-responsive"
+  
+          
 
 
 getBootstrap :: Html
 getBootstrap = link ! href "http://bootswatch.com/journal/bootstrap.min.css" ! rel "stylesheet"
-
-wrapInContainerFluid :: Html -> Html
-wrapInContainerFluid html = do
-  H.div ! class_ "container-fluid" $ html
