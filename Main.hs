570da1881d3475b25c5a340fb4284a4c564582de
Alex Recker <alex@reckerfamily.com>
2014-10-07 17:10:10 -0500

    YES YES YES YES YES

--- Main.hs
+++ Main.hs
@@ -1,17 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Applicative
 import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow
 import qualified Data.Text as T
 import Control.Monad
 
 
+data Comic = Comic { title :: String, link :: String } deriving (Show)
+
+instance FromRow Comic where
+	fromRow = Comic <$> field <*> field
+
+
 main = do
 	conn <- open "comics.db"
-	xs <- query_ conn "select created, title, link from comics"
-	forM_ xs $ \(created, title, link) ->
-		putStrLn $ T.unpack title ++ " : " ++ link ++ " : " ++ created
+	let results = getAllComics conn
+	close conn
 
 
-getAllComicsInOrder = do
-	conn <- open "comics.db"
-	query_ conn "SELECT created, title, link FROM comics"
+getAllComics :: Connection -> IO [Comic]
+getAllComics connection = do
+	let q = stringToQuery "SELECT title, link FROM comics"
+	query_ connection q :: IO [Comic]
+
+
+getTitle :: Comic -> String
+getTitle (Comic { title=t, link=l}) = t
+
+
+getLink :: Comic -> String
+getLink (Comic { title=t, link=l}) = l
+	
+
+stringToQuery :: [Char] -> Query
+stringToQuery a = Query (T.pack a)
