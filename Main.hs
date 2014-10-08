58fc02e1913d21f15758c432c4c31022e70ceb5e
Alex Recker <alex@reckerfamily.com>
2014-10-08 09:27:56 -0500

    INTEGRATION COMPLETE

--- Main.hs
+++ Main.hs
@@ -1,53 +1,118 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import Data.Monoid (mconcat)
+import Control.Monad (forM_)
+import Text.Blaze.Html5
+import Text.Blaze.Html5.Attributes
+import qualified Web.Scotty as S
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Html.Renderer.Text
 import Control.Applicative
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
 import qualified Data.Text as T
+import qualified Database.SQLite.Simple as DB
 import Control.Monad
-
 
 
 data Comic = Comic { id :: Int, created :: String, title :: String, link :: String } deriving (Show)
 
 instance FromRow Comic where
-	fromRow = Comic <$> field <*> field <*> field <*> field
+  fromRow = Comic <$> field <*> field <*> field <*> field
 
 
 main = do
-	conn <- open "comics.db"
-	let results = getAllComics conn
-	close conn
+  scotty 3000 $ do
+    get "/" $ do
+      S.html . renderHtml $ do getHome
+
+    get "/archives/" $ do
+      S.html . renderHtml $ do getArchives
 
 
+getHome :: Html
+getHome = do
+  H.head $ do
+    meta ! charset "UTF-8"
+    H.title "Comics, Man"
+    getCSS
+  H.body $ do
+    H.div ! class_ "container-fluid main" $ do
+      getJumbotron
+      getNav
+      getLatest
+
+
+getArchives :: Html
+getArchives = do
+  H.head $ do
+    meta ! charset "UTF-8"
+    H.title "Comics, Man | Archives"
+    getCSS
+  H.body $ do
+    H.div ! class_ "container-fluid main" $ do
+      getJumbotron
+      getNav
+      H.p "hello"
+
+-- DB  -------------------
 getAllComics :: Connection -> IO [Comic]
 getAllComics connection = do
-	let q = stringToQuery "SELECT id, created, title, link FROM comics"
-	query_ connection q :: IO [Comic]
+  let q = stringToQuery "SELECT id, created, title, link FROM comics"
+  DB.query_ connection q :: IO [Comic]
 
 
 getComicById :: Connection -> Int -> IO [Comic]
 getComicById connection id = do
-	let q = stringToQuery ("SELECT id, created, title, link FROM comics WHERE id = '" ++ (show id) ++ "'")
-	results <- query_ connection q :: IO [Comic]
-	return results
+  let q = stringToQuery ("SELECT id, created, title, link FROM comics WHERE id = '" ++ (show id) ++ "'")
+  results <- DB.query_ connection q :: IO [Comic]
+  return results
 
 
 getLatestComic :: Connection -> IO [Comic]
 getLatestComic connection = do
-	let q = stringToQuery ("SELECT id, created, title, link FROM comics ORDER BY id DESC LIMIT 1")
-	results <- query_ connection q :: IO [Comic]
-	return results
+  let q = stringToQuery ("SELECT id, created, title, link FROM comics ORDER BY id DESC LIMIT 1")
+  results <- DB.query_ connection q :: IO [Comic]
+  return results
 
-
-getTitle :: Comic -> String
-getTitle (Comic { title=t, link=l}) = t
-
-
-getLink :: Comic -> String
-getLink (Comic { title=t, link=l}) = l
-	
 
 stringToQuery :: [Char] -> Query
 stringToQuery a = Query (T.pack a)
+
+
+-- Object Helpers --------
+getTitle :: Comic -> String
+getTitle (Comic { Main.title=t, Main.link=l}) = t
+
+
+getLink :: Comic -> String
+getLink (Comic { Main.title=t, Main.link=l}) = l
+
+-- HTML Helpers ----------
+
+getJumbotron :: Html
+getJumbotron = H.div ! class_ "jumbotron" $ h1 "Comics, man."
+
+
+getNav :: Html
+getNav = do
+  H.div ! class_ "nav nav-pills" $ do
+    li ! class_ "active" $ a ! href "/" $ "Latest"
+    li $ a ! href "/archives/" $ "Everything"
+    li $ a ! href "/follow/" $ "Follow"
+
+
+getLatest :: Html
+getLatest = do
+  H.div ! class_ "page-header" $ do
+      h1 ! class_ "lead" $ "Mitochondrial Eve"
+      p "October 5, 2014"
+  img ! src "http://www.explosm.net/db/files/Comics/Dave/EVE.png" ! alt "" ! class_ "img-responsive"
+  
+          
+getCSS :: Html
+getCSS = do
+  H.link ! href "http://bootswatch.com/journal/bootstrap.min.css" ! rel "stylesheet"
+  H.style ".main {max-width: 500px;margin-bottom: 3em;} .jumbotron {background-color: transparent;}"
